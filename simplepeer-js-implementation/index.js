f47f978795bd40d492a3aea01a9fab89613a6b31
Jaskaran Sandhu <79944764+jsan956-ai@users.noreply.github.com>
2023-09-01 16:59:11 +1200

    signal server working, updated ui so that id doesnt have to be entered manually, connection successful message displays

--- simplepeer-js-implementation/index.js
+++ simplepeer-js-implementation/index.js
@@ -1,25 +1,26 @@
-import getUserMedia from 'getusermedia';
-import Peer from 'simple-peer';
+import getUserMedia from "getusermedia";
+import Peer from "simple-peer";
+
+const MESSAGE_TIMEOUT = 3000;
 
 let ws;
 let peer;
 
 // Wait for the DOM to load before initializing WebSocket and Peer connections
 document.addEventListener("DOMContentLoaded", () => {
-  ws = new WebSocket('ws://localhost:8080'); // Connect to signaling server at port 8080
-  
+  ws = new WebSocket("ws://localhost:8080"); // Connect to signaling server at port 8080
+
   ws.onopen = () => {
-    console.log('Connected to the signaling server');
-    // Initialize peer only after WebSocket connection is established
+    console.log("Connected to the signaling server");
     initializePeer();
+    showConnectionStatus(true);
   };
-  
-  // message received from signal server as blob, parsing to text
-  ws.onmessage = message => {
+
+  ws.onmessage = (message) => {
     if (message.data instanceof Blob) {
       const reader = new FileReader();
       reader.readAsText(message.data);
-      reader.onload = function() {
+      reader.onload = () => {
         const data = JSON.parse(reader.result);
         peer.signal(data);
       };
@@ -28,34 +29,54 @@ document.addEventListener("DOMContentLoaded", () => {
       peer.signal(data);
     }
   };
-  
 });
 
-function initializePeer() {
+const initializePeer = () => {
   getUserMedia({ video: true, audio: false }, (err, stream) => {
     if (err) {
       console.error(`Error in getUserMedia: ${err}`);
       return;
     }
-  
-    const localVideo = document.getElementById('localVideo');
+
+    const localVideo = document.getElementById("localVideo");
     localVideo.srcObject = stream;
-  
+
     peer = new Peer({
-      initiator: location.hash === '#init',
+      initiator: location.hash === "#init",
       trickle: false,
       stream,
     });
-  
-    peer.on('error', err => console.error(`Error in peer: ${err}`));
-  
-    peer.on('signal', data => {
-      ws.send(JSON.stringify(data)); // Send signal data when WebSocket is ready
+
+    peer.on("error", (err) => {
+      console.error(`Error in peer: ${err}`);
+      showConnectionStatus(false);
     });
-  
-    peer.on('stream', stream => {
-      const remoteVideo = document.getElementById('remoteVideo');
+
+    peer.on("signal", (data) => {
+      ws.send(JSON.stringify(data));
+    });
+
+    peer.on("stream", (stream) => {
+      const remoteVideo = document.getElementById("remoteVideo");
       remoteVideo.srcObject = stream;
     });
   });
-}
+};
+
+const showConnectionStatus = (successful) => {
+  const connectionStatusDiv = document.querySelector(".connection-success");
+
+  if (successful) {
+    connectionStatusDiv.style.backgroundColor = "green";
+    connectionStatusDiv.innerHTML = "<p>Connection successful!</p>";
+  } else {
+    connectionStatusDiv.style.backgroundColor = "red";
+    connectionStatusDiv.innerHTML = "<p>Connection failed!</p>";
+  }
+
+  connectionStatusDiv.style.display = "block";
+
+  setTimeout(() => {
+    connectionStatusDiv.style.display = "none";
+  }, MESSAGE_TIMEOUT);
+};
