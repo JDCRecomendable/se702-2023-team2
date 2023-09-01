c9b31fe2777834ade9d159366035e4eeeb20e899
Jaskaran Sandhu <79944764+jsan956-ai@users.noreply.github.com>
2023-09-01 16:13:48 +1200

    simple implementation for a signal server to automatically establish the connection

---
+++ simplepeer-js-implementation/server.js
@@ -0,0 +1,20 @@
+import WebSocket from 'ws';
+
+/**
+ * Signal server simple implementation to establish the connection between the two peers.
+ * After the initial connection is established, the signal server is not used further.
+ */
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', ws => {
+  ws.on('message', message => {
+
+    // it simply broadcasts the message sent by one peer to all other peers.
+    // Only text messages are required.
+    wss.clients.forEach(client => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
